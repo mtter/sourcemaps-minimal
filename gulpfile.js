276ea3002dbb12d15fd14e5afc124408be3d66b1
mtter <mtter@users.noreply.github.com>
2017-06-28 02:33:52 +0200

    Added extended minimal sample with included partials and mapped souces. Added simple sample page for viewing result in dev tools.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,13 @@ var cleanCSS=require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('minimal', function() {
-	return gulp.src(['./minimal*.scss'])
+	return gulp.src(['./minimal-*.scss'])
         .pipe(sourcemaps.init())
         .pipe(sass())
 		.pipe(cleanCSS())
+		.pipe(sourcemaps.mapSources(function(sourcePath, file) {
+			return '/src/scss/' + sourcePath;
+		}))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('out'));
 })
